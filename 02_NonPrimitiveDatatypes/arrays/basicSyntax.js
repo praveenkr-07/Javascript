60bff0322d23ddf13478bfdd64ca43137c77ea45
praveenkr-07 <praveenmahour098@outlook.com>
2026-07-06 17:50:39 +0530

    Update basicSyntax.js

--- 02_NonPrimitiveDatatypes/arrays/basicSyntax.js
+++ 02_NonPrimitiveDatatypes/arrays/basicSyntax.js
@@ -12,10 +12,10 @@
 //// for ... of loop in js  => for values at each index in arr
 //for(let value of studentDetails) console.log(value);
 
-//// for ... in loop in js  => give index 
+//// for ... in loop in js  => give index
 //for(let index in studentDetails) console.log(index);
 
-//// if we have to print both index and values in pair 
+//// if we have to print both index and values in pair
 //for(let index in studentDetails) console.log(`${index} : ${studentDetails[index]}`);
 
 //let arr1 = [10,20,30];
@@ -27,7 +27,6 @@
 // arr1.push(40);
 // console.log(arr1.length);
 // for(let index in arr1) console.log(`${index} : ${arr1[index]}`);
-
 
 // // push an element from the front
 // arr1.unshift(5);
@@ -44,7 +43,6 @@
 // console.log(arr1.length);
 // for(let index in arr1) console.log(`${index} : ${arr1[index]}`);
 
-
 // // to check if an element present in the arrays or not
 // console.log(arr1.includes(10));
 // console.log(arr1.includes(50));
@@ -59,7 +57,7 @@
 
 // // split
 // let arr3 = "Praveen-Kumar-is-a-graduate-student-in-DTU";
-// console.log(arr3.split("-")); 
+// console.log(arr3.split("-"));
 // // [
 // //   'Praveen',  'Kumar',
 // //   'is',       'a',
@@ -76,10 +74,10 @@
 //     console.log(`${index} : ${temparr[index]} `);
 // }
 
-// //splice 
+// //splice
 // let arr5 = [10,20,30,40,50,60,70];
 // arr5.splice(1,4);
-// // remove the values from index 1 to 4 in original array 
+// // remove the values from index 1 to 4 in original array
 // //doesn't return the copy , change in original array
 // for(let index in arr5){
 //     console.log(`${index} : ${arr5[index]} `);
@@ -92,8 +90,8 @@ let programmingLanguages = ["C++", "Java", "Python"];
 // Arguments: start at index 1, delete 0 items, insert "JavaScript"
 // programmingLanguages.splice(1, 0, "JavaScript");
 
-// console.log(programmingLanguages); 
-// // Output: ["C++", "JavaScript", "Java", "Python"] 
+// console.log(programmingLanguages);
+// // Output: ["C++", "JavaScript", "Java", "Python"]
 
 // // delete using splice
 // let fruits = ["Apple", "Banana", "Mango"];
@@ -102,16 +100,26 @@ let programmingLanguages = ["C++", "Java", "Python"];
 // // Arguments: start at index 1, delete 1 item, insert "Orange"
 // fruits.splice(1, 1, "Orange");
 
-// console.log(fruits); 
+// console.log(fruits);
 // // Output: ["Apple", "Orange", "Mango"]
 
+// whatever we can do with the help of splice ; can also be done
+// with the help of slice (some exceptions like insertion) .
+// but main difference is slice returns
+// a copy after changes while slice do changes in the original array
 
-// whatever we can do with the help of splice ; can also be done 
-// with the help of slice (some exceptions like insertion) . 
-// but main difference is slice returns 
-// a copy after changes while slice do changes in the original array 
+let arr = [1, 101, 75, 50, 31, 25, -1, 0, -34];
+arr.sorting = function (compare) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (compare(arr[j], arr[j + 1]) > 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
 
-let  arr = [1, 101, 75, 50, 31, 25, -1, 0, -34];
-arr.sort((a,b)=> a-b);
+arr.sorting((a, b) => a - b);
 console.log(arr);
 // (a,b)=> a-b : arrow funciton
